8e188abbc309e06056a1126e732da66143d4e91a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-19 13:00:17 +0100

    Add files via upload

---
+++ form.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class myapp:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("hello")
+        self.root.configure(bg="black")
+
+
+root=tk.Tk()
+app=myapp(root)
+root.mainloop()
+
